7411386bb7d84506f1c4b1fdd47dd2b64ca7603c
Ernest Pokropek <ernest.pokropek98@gmail.com>
2022-11-26 16:44:29 +0100

    fixed minor bug, last question not finished

--- lab2.py
+++ lab2.py
@@ -7,16 +7,25 @@ from Functions import *
 from gaussfft import gaussfft
 
 
-def deltax(sobel=False):
+def deltax(sobel=False, reduced=False):
     if sobel:
         return np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    
+    if reduced:   
+        dxmask = np.array([[1/2, 0, -1/2]])
+        return convolve2d(np.array([[0, 1, 0]]).T, dxmask)
     
     dxmask = np.array([[0, 1/2, 0, -1/2, 0]])
     return convolve2d(np.array([[0, 0, 1, 0, 0]]).T, dxmask)
 
-def deltay(sobel=False):
+def deltay(sobel=False, reduced=False):
     if sobel:
         return np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    
+    if reduced:   
+        dymask = np.array([[1/2, 0, -1/2]]).T
+        return convolve2d(np.array([[0, 1, 0]]), dymask)
+    
     dymask = np.array([[0, 1/2, 0, -1/2, 0]]).T
     return convolve2d(np.array([[0, 0, 1, 0, 0]]), dymask)
 
@@ -38,13 +47,13 @@ DELTA_XXY = convolve2d(DELTA_XX, DELTA_Y, "same")
 DELTA_XYY = convolve2d(DELTA_X, DELTA_YY, "same")
 DELTA_YYY = convolve2d(DELTA_Y, DELTA_YY, "same")
 
-def Lv(inpic, shape = 'same', sigma=None, sobel=False):
+def Lv(inpic, shape = 'same', sigma=None, sobel=False, reduced=True):
     
     if sigma is not None:
         inpic = gaussfft(inpic, sigma)
     
-    Lx = convolve2d(inpic, deltax(sobel), shape)
-    Ly = convolve2d(inpic, deltax(sobel), shape)
+    Lx = convolve2d(inpic, deltax(sobel, reduced), shape)
+    Ly = convolve2d(inpic, deltay(sobel, reduced), shape)
     return np.sqrt(Lx**2 + Ly**2)
 
 def Lvvtilde(inpic, shape = 'same'):
@@ -67,15 +76,107 @@ def Lvvvtilde(inpic, shape = 'same'):
     return (Lx**3)*Lxxx + 3*(Lx**2)*Ly*Lxxy + 3*Lx*(Ly**2)*Lxyy + (Ly**3)*Lyyy
 	
 
-def extractedge(inpic, scale, threshold, shape):
-    return contours
+def extractedge(inpic, scale, threshold, shape='same'):
+    
+    inpic_gauss = discgaussfft(inpic, sigma2=scale)
+    zeropic = Lvvtilde(inpic_gauss, shape=shape)
+    maskpic1 = Lvvvtilde(inpic_gauss, shape=shape) < 0
+    
+    curves = zerocrosscurves(zeropic, maskpic1)
+    
+    maskpic2 = Lv(inpic_gauss, shape=shape) > threshold
+    curves = thresholdcurves(curves, maskpic2)
+
+    return curves
 
 def houghline(curves, magnitude, nrho, ntheta,
               threshold, nlines = 20, verbose = False):
+    
+    # Allocate accumulator space
+    acc = np.zeros((nrho, ntheta))
+    
+    # Define a coordinate system in the accumulator space
+    length_x, length_y = magnitude.shape #magnitude.shape[0] / 2, magnitude.shape[1] / 2
+    r = np.sqrt(length_x**2 + length_y**2)
+    rho_vals = np.linspace(-r, r, nrho)
+    theta_vals = np.linspace(-np.pi/2, np.pi/2, ntheta)
+    # Loop over all the edge points
+    for edgepoint in zip(curves[0], curves[1]):
+        # Check if valid point with respect to threshold
+        x, y = edgepoint
+        
+        # Optionally, keep value from magnitude image
+        curve_magnitude = magnitude[x,y]
+        if curve_magnitude < threshold:
+            continue
+
+        # Loop over a set of theta values
+        for j, theta in enumerate(theta_vals):
+            
+            # Compute rho for each theta value
+            rho = (x - 0) * np.cos(theta) + (y - 0) * np.sin(theta)
+            
+            # Compute index values in the accumulator space
+            i = np.argmin(abs(rho_vals - rho))
+            
+            # Update the accumulator
+            acc[i,j] += 1
+            
+            
+    # Extract local maxima from the accumulator
+    pos, value, _ = locmax8(acc)
+    
+    # Delimit the number of responses if necessary
+    indexvector = np.argsort(value)[-nlines:]
+    pos = pos[indexvector]
+    
+    linepar = []
+    
+    if verbose:  
+        fig = plt.figure()
+        # Overlay these curves on the gradient magnitude image
+        showgrey(magnitude, False)
+
+
+    for idx in range(nlines):
+        thetaidxacc = pos[idx, 0]
+        rhoidxacc = pos[idx, 1]
+        
+        max_theta = theta_vals[thetaidxacc]
+        max_rho = rho_vals[rhoidxacc]
+        
+        linepar.append([max_theta, max_rho])
+    
+        if verbose:            
+            # Compute a line for each one of the strongest responses in the accumulator
+            x0 = max_rho * np.cos(max_theta) + 0
+            y0 = max_rho * np.sin(max_theta) + 0
+            
+            if abs(np.sin(max_theta)) < 1e3:
+                dx = length_x*(-np.cos(max_theta)/np.sin(max_theta))
+            else: dx = 0
+            if abs(np.cos(max_theta)) < 1e5:
+                dy = length_y*(-np.sin(max_theta)/np.cos(max_theta))
+            else: dy = 0
+            
+            dx = r * (-np.sin(max_theta))
+            dy = r * (np.cos(max_theta))
+            
+            plt.plot([y0-dy, y0, y0+dy], [x0-dx, x0, x0+dx], 'r-')
+    
+    if verbose:
+        plt.show()
+            
+    # Return the output data [linepar, acc]
     return linepar, acc
 
 def houghedgeline(pic, scale, gradmagnthreshold, nrho,
                   ntheta, nlines = 20, verbose = False):
+    curves = extractedge(pic, scale, gradmagnthreshold, "same")
+    gaussianSmooth = discgaussfft(pic, scale)
+    gradmagn = Lv(gaussianSmooth, "same")
+
+    linepar, acc = houghline(curves, gradmagn, nrho, ntheta, gradmagnthreshold, nlines, verbose)
     return linepar, acc
 
 			
@@ -144,14 +245,88 @@ def ex3():
     print(convolve2d(x**3, DELTA_XX, "valid"))
     print(6*x)
     print(convolve2d(x**2*y, DELTA_XXY, "valid"))
-   
+    
+    house = np.load("Images-npy/godthem256.npy")
+    scales = [0.0001, 1.0, 4.0, 16.0, 64.0]
+    
+    fig = plt.figure()
+    for i, scale in enumerate(scales):
+        ax = fig.add_subplot(1, len(scales), i+1)
+        showgrey(contour(Lvvtilde(discgaussfft(house, scale), 'same')), False)
+        ax.set_title(f"scale={scale}")
+        
+    plt.show()
+    
+    tools = np.load("Images-npy/few256.npy")
+        
+    fig = plt.figure()
+    for i, scale in enumerate(scales):
+        ax = fig.add_subplot(1, len(scales), i+1)
+        showgrey((Lvvvtilde(discgaussfft(tools, scale), 'same')<0).astype(int), False)
+        ax.set_title(f"scale={scale}")
+        
+    plt.show()
+    
+    
+def ex4():
+    
+    imgs = [np.load("Images-npy/few256.npy"), np.load("Images-npy/godthem256.npy")]
+    
+    scales = [1.0, 1.5, 2.0, 4.0, 8.0]
+    thresholds = [2, 5, 8, 10]
+    
+    # selection of scale and threshold
+    for img in imgs:
+        
+        fig = plt.figure()
+        for j, threshold in enumerate(thresholds):
+            for i, scale in enumerate(scales):
+                ax = fig.add_subplot(len(thresholds), len(scales), j*len(scales) +i +1)
+                edgecurves = extractedge(img, scale=scale, threshold=threshold)
+                overlaycurves(img, edgecurves)
+                ax.set_title(f"t={threshold}, scale={scale}")
+        
+        plt.show()
+
+
+    # best results
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 2,1)
+    edgecurves = extractedge(imgs[0], scale=8, threshold=2)
+    overlaycurves(imgs[0], edgecurves)
+    ax.set_title(f"t={2}, scale={8}")
+        
+    ax = fig.add_subplot(1, 2,2 )
+    edgecurves = extractedge(imgs[1], scale=8, threshold=2)
+    overlaycurves(imgs[1], edgecurves)
+    ax.set_title(f"t={2}, scale={8}")
+    
+         
+    plt.show()
+
+def ex5():
+    testimage1 = np.load("Images-npy/triangle128.npy")
+    smalltest1 = binsubsample(testimage1)
+    
+    testimage2 = np.load("Images-npy/houghtest256.npy")
+    smalltest2 = binsubsample(binsubsample(testimage2))
+    
+    showgrey(smalltest1)
+    
+    linepar, acc = houghedgeline(smalltest1, 1, 20, 400, 300, 20, True)
+
+
 if __name__=='__main__':
     
-    exercise = 3 
+    exercise = 5
     if exercise == 1:
         ex1()
     elif exercise == 2:
         ex2(True, 4)
     elif exercise == 3:
         ex3()
+    elif exercise == 4:
+        ex4()
+    elif exercise == 5:
+        ex5()
         
